2cbce7b511f4df2bb143261cf0aea8ef72b17c02
吴志伟 <2465178407@qq.com>
2020-07-04 09:54:06 +0800

    完成about界面

--- app/src/main/java/com/thirtyseven/studenthelp/ui/me/AboutActivity.java
+++ app/src/main/java/com/thirtyseven/studenthelp/ui/me/AboutActivity.java
@@ -1,8 +1,10 @@
 package com.thirtyseven.studenthelp.ui.me;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.thirtyseven.studenthelp.R;
 
@@ -13,5 +15,19 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         setTitle(R.string.title_about);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
