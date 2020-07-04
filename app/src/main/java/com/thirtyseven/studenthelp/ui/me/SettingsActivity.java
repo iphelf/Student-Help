e0494206e97bac00d12e40aa91bc86899140a8ff
吴志伟 <2465178407@qq.com>
2020-07-04 11:34:43 +0800

    完成announcement界面

--- app/src/main/java/com/thirtyseven/studenthelp/ui/me/SettingsActivity.java
+++ app/src/main/java/com/thirtyseven/studenthelp/ui/me/SettingsActivity.java
@@ -1,8 +1,10 @@
 package com.thirtyseven.studenthelp.ui.me;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -20,7 +22,11 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         setTitle(R.string.title_settings);
-
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
         skinSpinner=findViewById(R.id.spinner_skin);
         skinSpinner.setPrompt("请选择更换的颜色");
         skinList=new ArrayList<CharSequence>();
@@ -32,5 +38,13 @@ public class SettingsActivity extends AppCompatActivity {
         skinAdapter = new ArrayAdapter<CharSequence>(this,android.R.layout.simple_spinner_item,skinList);
         skinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         skinSpinner.setAdapter(skinAdapter);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
