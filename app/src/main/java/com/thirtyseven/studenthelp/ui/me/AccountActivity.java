087572db4d5e90e577caec0ed0565bd1e1add2eb
吴志伟 <2465178407@qq.com>
2020-07-04 20:32:01 +0800

    完成account界面

--- app/src/main/java/com/thirtyseven/studenthelp/ui/me/AccountActivity.java
+++ app/src/main/java/com/thirtyseven/studenthelp/ui/me/AccountActivity.java
@@ -2,9 +2,21 @@ package com.thirtyseven.studenthelp.ui.me;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.thirtyseven.studenthelp.R;
+import com.thirtyseven.studenthelp.ui.common.ErrandActivity;
+import com.thirtyseven.studenthelp.ui.common.ZoneActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AccountActivity extends AppCompatActivity {
 
@@ -13,5 +25,29 @@ public class AccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
         setTitle(R.string.title_account);
+        String[] values = {
+                "家乡", "生日", "性别", "姓名", "学号"
+        };
+        int[] fieldIds = {
+                R.id.textView_tag
+        };
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        for(int i=0;i<values.length;i++){
+            Map<String, Object> map = new HashMap<>();
+            map.put("title", values[i]);
+            mapList.add(map);
+        }
+
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(
+                this,
+                mapList,
+                R.layout.listviewitem_account,
+                new String[]{"title"},
+                fieldIds
+        );
+        ListView listViewAccountList = findViewById(R.id.listView_account);
+        listViewAccountList.setAdapter(simpleAdapter);
+
     }
 }
