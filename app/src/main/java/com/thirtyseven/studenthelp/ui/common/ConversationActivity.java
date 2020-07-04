b4f4680a1520048f5a5b4787fe7a931eb133a5d1
吴志伟 <2465178407@qq.com>
2020-07-04 08:54:01 +0800

    完成会话界面

--- app/src/main/java/com/thirtyseven/studenthelp/ui/common/ConversationActivity.java
+++ app/src/main/java/com/thirtyseven/studenthelp/ui/common/ConversationActivity.java
@@ -1,20 +1,77 @@
 package com.thirtyseven.studenthelp.ui.common;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.thirtyseven.studenthelp.R;
+import com.thirtyseven.studenthelp.data.MsgInfo;
+import com.thirtyseven.studenthelp.ui.common.adapter.MessageAdapter;
 
 public class ConversationActivity extends AppCompatActivity {
+    private EditText msg_edit;
+    private Button left_btn;
+    private Button right_btn;
+    private String msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
         setTitle(R.string.title_conversation);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        init();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void init() {
+        msg_edit = findViewById(R.id.editText_message);
+        left_btn = findViewById(R.id.btn_left);
+        right_btn = findViewById(R.id.btn_right);
+
+        final ListView list_conversation = findViewById(R.id.listView_conversation);
+        ;
+        final MessageAdapter adapter = new MessageAdapter(ConversationActivity.this);
+        list_conversation.setAdapter(adapter);
+        left_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                msg = msg_edit.getText().toString().trim();
+                adapter.addDataToAdapter(new MsgInfo(msg, null));
+                adapter.notifyDataSetChanged();
+                list_conversation.smoothScrollToPosition(list_conversation.getCount() - 1);
+                msg_edit.setText("");
+            }
+        });
+        right_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                msg = msg_edit.getText().toString().trim();
+                adapter.addDataToAdapter(new MsgInfo(null, msg));
+                adapter.notifyDataSetChanged();
+                list_conversation.smoothScrollToPosition(list_conversation.getCount() - 1);
+                msg_edit.setText("");
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/thirtyseven/studenthelp/ui/common/adapter/MessageAdapter.java
@@ -0,0 +1,99 @@
+package com.thirtyseven.studenthelp.ui.common.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.thirtyseven.studenthelp.R;
+import com.thirtyseven.studenthelp.data.MsgInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageAdapter extends BaseAdapter {
+    private Context context;
+    private List<MsgInfo> datas = new ArrayList<>();
+
+    private ViewHolder viewHolder;
+
+    public MessageAdapter(Context context) {
+        this.context = context;
+    }
+    public void addDataToAdapter(MsgInfo e) {
+        datas.add(e);
+    }
+
+    @Override
+    public int getCount() {
+        return datas.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return datas.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+
+            LayoutInflater inflater = LayoutInflater.from(context);
+            convertView = inflater.inflate(R.layout.listviewitem_conversation, null);
+            viewHolder = new ViewHolder(convertView);
+
+            convertView.setTag(viewHolder);
+
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        //获取adapter中的数据
+        String left = datas.get(position).getLeft_text();
+        String right = datas.get(position).getRight_text();
+
+        //如果数据为空，则将数据设置给右边，同时显示右边，隐藏左边
+        if (left == null) {
+            viewHolder.text_right.setText(right);
+            viewHolder.right.setVisibility(View.VISIBLE);
+            viewHolder.left.setVisibility(View.INVISIBLE);
+        }
+
+        //与上一步相反
+        if (right == null) {
+            viewHolder.text_left.setText(left);
+            viewHolder.left.setVisibility(View.VISIBLE);
+            viewHolder.right.setVisibility(View.INVISIBLE);
+        }
+
+        return convertView;
+    }
+
+    public static class ViewHolder {
+        public View rootView;
+        public TextView text_left;
+        public LinearLayout left;
+        public TextView text_right;
+        public LinearLayout right;
+
+        public ViewHolder(View rootView) {
+            this.rootView = rootView;
+            this.text_left = (TextView) rootView.findViewById(R.id.textView_oneWord);
+            this.left = (LinearLayout) rootView.findViewById(R.id.linearLayout_left);
+            this.text_right = (TextView) rootView.findViewById(R.id.textView_twoWord);
+            this.right = (LinearLayout) rootView.findViewById(R.id.linearLayout_right);
+        }
+
+    }
+
+}
